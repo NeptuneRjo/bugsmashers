f4a227527c90891c1303955ce5bd8ebbcaf4e5b1
NeptuneRjo <rjo9000@outlook.com>
2023-06-26 16:48:15 -0400

    DataContext

---
+++ BugTrackerMvc/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Issue>()
+                .HasOne(e => e.Status)
+                .WithOne(e => e.Issue)
+                .HasForeignKey<Status>(e => e.IssueId);
+        }
+    }
+}
