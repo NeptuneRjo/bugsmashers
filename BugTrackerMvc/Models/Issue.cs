b5e97c81273bc2d541583298b69cb8adebfdeedf
NeptuneRjo <rjo9000@outlook.com>
2023-06-26 16:48:19 -0400

    Add models

---
+++ BugTrackerMvc/Models/Issue.cs
@@ -0,0 +1,11 @@
+﻿namespace BugTracker.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Status? Status { get; set; }
+        public string Assignee { get; set; }
+    }
+}
---
+++ BugTrackerMvc/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿namespace BugTracker.Models
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public bool New { get; set; }
+        public bool Open { get; set; }
+        public bool Resolved { get; set; }
+        public bool Closed { get; set; }
+        public int IssueId { get; set; } // Required foreign key
+        public Issue Issue { get; set; } = null!; // Required reference navigation to principle
+
+    }
+}
