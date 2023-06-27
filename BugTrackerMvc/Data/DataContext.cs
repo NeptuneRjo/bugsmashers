ae77506947c891ef6290c665c5d7e8655db232d2
NeptuneRjo <rjo9000@outlook.com>
2023-06-27 02:49:16 -0400

    Fix syntax for imports

--- BugTrackerMvc/Data/DataContext.cs
+++ BugTrackerMvc/Data/DataContext.cs
@@ -1,21 +1,21 @@
-﻿using BugTracker.Models;
+﻿using BugTrackerMvc.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BugTracker.Data
+namespace BugTrackerMvc.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         
         public DbSet<Issue> Issues { get; set; }
-        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Issue>()
-                .HasOne(e => e.Status)
+                .HasMany(e => e.Comments)
                 .WithOne(e => e.Issue)
-                .HasForeignKey<Status>(e => e.IssueId);
+                .HasForeignKey(e => e.IssueId);
         }
     }
 }
