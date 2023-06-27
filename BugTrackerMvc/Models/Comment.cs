9aa95c113c3fe50176da77a86395c09b3821e450
NeptuneRjo <rjo9000@outlook.com>
2023-06-27 02:48:56 -0400

    Restructure issues to include comments

---
+++ BugTrackerMvc/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace BugTrackerMvc.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public string? Author { get; set; }
+
+        // Required foreign key
+        public int IssueId { get; set; } 
+        // Required reference navigation to principle
+        public Issue Issue { get; set; } = null!; 
+    }
+}
--- BugTrackerMvc/Models/Issue.cs
+++ BugTrackerMvc/Models/Issue.cs
@@ -1,11 +1,19 @@
-﻿namespace BugTracker.Models
+﻿
+namespace BugTrackerMvc.Models
 {
     public class Issue
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
-        public Status? Status { get; set; }
-        public string Assignee { get; set; }
+        public bool Solved { get; set; }
+        public string Poster { get; set; }
+
+        public ICollection<Comment> Comments { get; } = new List<Comment>();
+
+        public Issue()
+        {
+            Solved = false;
+        }
     }
 }
