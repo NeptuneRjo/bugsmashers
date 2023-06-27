0882121704ceb737e39f1fb9e061c992eae875af
NeptuneRjo <rjo9000@outlook.com>
2023-06-27 02:49:02 -0400

    Migrations

--- BugTrackerMvc/Migrations/20230627063937_AddComments.cs
+++ BugTrackerMvc/Migrations/20230627063937_AddComments.cs
@@ -5,7 +5,7 @@
 namespace BugTrackerMvc.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class AddComments : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -16,9 +16,10 @@ namespace BugTrackerMvc.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Assignee = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    Solved = table.Column<bool>(type: "bit", nullable: false),
+                    Poster = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -26,22 +27,20 @@ namespace BugTrackerMvc.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "Statuses",
+                name: "Comments",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    New = table.Column<bool>(type: "bit", nullable: false),
-                    Open = table.Column<bool>(type: "bit", nullable: false),
-                    Resolved = table.Column<bool>(type: "bit", nullable: false),
-                    Closed = table.Column<bool>(type: "bit", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     IssueId = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                    table.PrimaryKey("PK_Comments", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_Statuses_Issues_IssueId",
+                        name: "FK_Comments_Issues_IssueId",
                         column: x => x.IssueId,
                         principalTable: "Issues",
                         principalColumn: "Id",
@@ -49,17 +48,16 @@ namespace BugTrackerMvc.Migrations
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Statuses_IssueId",
-                table: "Statuses",
-                column: "IssueId",
-                unique: true);
+                name: "IX_Comments_IssueId",
+                table: "Comments",
+                column: "IssueId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Statuses");
+                name: "Comments");
 
             migrationBuilder.DropTable(
                 name: "Issues");
