83cc456710b4d51cf04e5bbb1255341494bc5d94
NeptuneRjo <rjo9000@outlook.com>
2023-06-28 15:35:15 -0400

    Remove unused dependencies

--- BugTrackerMvc/Controllers/AuthenticationController.cs
+++ BugTrackerMvc/Controllers/AuthenticationController.cs
@@ -1,10 +1,7 @@
-﻿using AspNet.Security.OAuth.GitHub;
-using BugTrackerMvc.Extensions;
+﻿using BugTrackerMvc.Extensions;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 
 namespace BugTrackerMvc.Controllers
 {
