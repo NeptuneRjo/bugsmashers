24de407cb4359fb46eec39ce11aed10350bff502
NeptuneRjo <rjo9000@outlook.com>
2023-06-29 03:28:19 -0400

    Create interface for IssueRepository

---
+++ BugTrackerMvc/Interfaces/IIssueRepository.cs
@@ -0,0 +1,14 @@
+﻿using BugTrackerMvc.Models;
+
+namespace BugTrackerMvc.Interfaces
+{
+    public interface IIssueRepository
+    {
+        void InsertIssue(Issue Issue);
+        IEnumerable<Issue> GetIssues();
+        Issue GetIssueById(int? id);
+        bool IssueExists(int? id);
+        void UpdateIssue(Issue issue);
+        void DeleteIssue(int? id);
+    }
+}
