efc4b7ef67e65dd01b7046292296b492ffe809db
NeptuneRjo <rjo9000@outlook.com>
2023-07-01 23:11:21 -0400

    Implemented IDataContext to data context

--- BugTrackerMvc/Data/DataContext.cs
+++ BugTrackerMvc/Data/DataContext.cs
@@ -1,11 +1,14 @@
-﻿using BugTrackerMvc.Models;
+﻿using BugTrackerMvc.Interfaces;
+using BugTrackerMvc.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BugTrackerMvc.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : DbContext, IDataContext
     {
+        public DataContext() { }
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
         
         public DbSet<Issue> Issues { get; set; }
         public DbSet<Comment> Comments { get; set; }
