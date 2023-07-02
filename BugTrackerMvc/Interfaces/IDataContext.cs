1023790f334c0ac40ebd4f69b91fcb24a717228b
NeptuneRjo <rjo9000@outlook.com>
2023-07-01 23:11:05 -0400

    Created interface for database context

---
+++ BugTrackerMvc/Interfaces/IDataContext.cs
@@ -0,0 +1,13 @@
+﻿using BugTrackerMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTrackerMvc.Interfaces
+{
+    public interface IDataContext
+    {
+        DbSet<Issue> Issues { get; set; }
+        DbSet<Comment> Comments { get; set; }
+
+        int SaveChanges();
+    }
+}
