dde719028bc06447a2d6eb5b028b27a7a84bdedb
NeptuneRjo <rjo9000@outlook.com>
2023-07-07 18:53:00 -0400

    Add functions for getting by poster

--- BugTrackerMvc/Interfaces/IIssueRepository.cs
+++ BugTrackerMvc/Interfaces/IIssueRepository.cs
@@ -7,9 +7,11 @@ namespace BugTrackerMvc.Interfaces
         void InsertIssue(Issue Issue);
         IEnumerable<Issue> GetIssues();
         Issue GetIssueById(int? id);
+        IEnumerable<Issue> GetIssuesByPoster(string poster);
         bool IssueExists(int? id);
         void UpdateIssue(Issue issue);
         void DeleteIssue(int? id);
         IEnumerable<Comment> GetComments(int? id);
+        IEnumerable<Comment> GetCommentByPoster(string poster);
     }
 }
