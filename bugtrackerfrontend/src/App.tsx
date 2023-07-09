1aa634af488ea5bf172f230cca39f1b690fb4a4e
NeptuneRjo <rjo9000@outlook.com>
2023-07-09 14:57:04 -0400

    add user state and implement the navbar

--- bugtrackerfrontend/src/App.tsx
+++ bugtrackerfrontend/src/App.tsx
@@ -1,22 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
+import { getUserDetails } from './API/authRequests';
 import './App.css';
 import Dashboard from './views/Dashboard';
+import Navbar from './views/Navbar';
 
 function App() {
     const navigate = useNavigate()
 
+    const [user, setUser] = useState<string | null>(null)
+
     useEffect(() => {
         navigate("/dashboard")
+
+        ; (async () => {
+            const fetchedUser = await getUserDetails()
+
+            setUser(fetchedUser)
+        })()
     }, [])
 
-  return (
-      <div className="app">
-          <Routes>
-              <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
+    return (
+        <div className="app">
+            <Navbar user={user} setUser={setUser} />
+            <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
     </div>
-  )
+    )
 }
 
 export default App;
