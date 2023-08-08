328d497bf027126af559ce2941210fec17563b04
NeptuneRjo <rjo9000@outlook.com>
2023-08-08 04:05:36 -0400

    create custom exception for notfound

---
+++ BugTrackerMvc/CustomExceptions/ObjectNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace BugTrackerMvc.CustomExceptions
+{
+    public class ObjectNotFoundException : Exception
+    {
+        public ObjectNotFoundException() { }
+
+        public ObjectNotFoundException(string message)
+            : base(message) { }
+
+        public ObjectNotFoundException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
