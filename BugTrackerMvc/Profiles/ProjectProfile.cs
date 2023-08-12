5ddff205bc61b3eea61cfa47f08a5b69cf000a7e
NeptuneRjo <rjo9000@outlook.com>
2023-08-12 05:22:01 -0400

    Create project profile

---
+++ BugTrackerMvc/Profiles/ProjectProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BugTrackerMvc.Models;
+
+namespace BugTrackerMvc.Profiles
+{
+    public class ProjectProfile : Profile
+    {
+        public ProjectProfile()
+        {
+            CreateMap<ProjectModel, Project>();
+            CreateMap<Project, ProjectDto>();
+        }
+    }
+}
