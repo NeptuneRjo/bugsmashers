620aa71c53b41b9ae417d0b49acfdb77410ff67f
NeptuneRjo <rjo9000@outlook.com>
2023-08-11 22:59:23 -0400

    Create project repository

---
+++ BugTrackerMvc/Interfaces/IProjectRepository.cs
@@ -0,0 +1,9 @@
+﻿using BugTrackerMvc.Models;
+
+namespace BugTrackerMvc.Interfaces
+{
+    public interface IProjectRepository : IRepository<Project>
+    {
+        Task<Issue> AddIssue(Project project, Issue issue);
+    }
+}
---
+++ BugTrackerMvc/Repository/ProjectRepository.cs
@@ -0,0 +1,35 @@
+﻿using BugTrackerMvc.Interfaces;
+using BugTrackerMvc.Models;
+
+namespace BugTrackerMvc.Repository
+{
+    public class ProjectRepository : Repository<Project>, IProjectRepository
+    {
+        private readonly IDataContext _context;
+
+        public ProjectRepository(IDataContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Issue> AddIssue(Project project, Issue issue)
+        {
+            //if (issue.Project == null)
+            //    throw new Exception("Projet must be defined");
+
+            //_context.Issues.Add(issue);
+
+            //project.Issues.Add(issue);
+
+            //_context.SaveChanges();
+
+            //return issue;
+            project.Issues.Add(issue);
+
+            _context.Projects.Update(project);
+            _context.SaveChanges();
+
+            return issue;
+        }
+    }
+}
