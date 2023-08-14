3dffee195eb7c863c8856174e64e8448061610fe
NeptuneRjo <rjo9000@outlook.com>
2023-08-14 05:47:47 -0400

    create API class for project-related requests

---
+++ BugTrackerFrontend/APIs/Projects.ts
@@ -0,0 +1,106 @@
+import { IProjects, IssueModel, ProjectModel } from "../types"
+
+export default class Projects implements IProjects {
+    private options: RequestInit = {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+
+    baseURL: string
+
+    constructor(baseURL: string) {
+        this.baseURL = baseURL
+
+    }
+
+    async getAll() {
+        const response = await fetch(`${this.baseURL}/api/projects`, this.options)
+        let data: Response | undefined = undefined
+
+        if (response.ok) {
+            data = await response.json()
+        }
+
+        return { status: response.status, data }
+    }
+
+    async get(id: number) { 
+        const response = await fetch(`${this.baseURL}/api/projects/${id}`, this.options)
+        let data: Response | undefined = undefined
+
+        if (response.ok) {
+            data = await response.json()
+        }
+
+        return { status: response.status, data }
+    }
+
+    async create(model: ProjectModel) {
+        const options: RequestInit = this.options
+
+        options.method = "POST"
+        options.credentials = "include"
+        options.body = JSON.stringify(model)
+
+        const response = await fetch(`${this.baseURL}/api/projects`, options)
+        let data: Response | undefined = undefined
+
+        if (response.ok) {
+            data = await response.json()
+        }
+
+        return { status: response.status, data }
+    }
+
+    async update(id: number, model: ProjectModel): Promise<any> {
+        const options: RequestInit = this.options
+
+        options.method = "PUT"
+        options.credentials = "include"
+        options.body = JSON.stringify(model)
+
+        const response = await fetch(`${this.baseURL}/api/projects/${id}`, options)
+        let data: Response | undefined = undefined
+
+        if (response.ok) {
+            data = await response.json()
+        }
+
+        return { status: response.status, data }
+    }
+
+    async delete(id: number): Promise<any> {
+        const options: RequestInit = this.options
+
+        options.method = "DELETE"
+        options.credentials = "include"
+
+        const response = await fetch(`${this.baseURL}/api/projects/${id}`, options)
+        let data: Response | undefined = undefined
+
+        if (response.ok) {
+            data = await response.json()
+        }
+
+        return { status: response.status, data }
+    }
+
+    async add(id: number, issueModel: IssueModel): Promise<any> {
+        const options: RequestInit = this.options
+
+        options.method = "POST"
+        options.credentials = "include"
+
+        const response = await fetch(`${this.baseURL}/api/projects/${id}`, options)
+        let data: Response | undefined = undefined
+
+        if (response.ok) {
+            data = await response.json()
+        }
+
+        return { status: response.status, data }
+    }
+}
+
