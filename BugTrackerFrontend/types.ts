c637056385915b4b64a9414a2b8bb229f154634c
NeptuneRjo <rjo9000@outlook.com>
2023-08-14 03:24:44 -0400

    Create types

---
+++ BugTrackerFrontend/types.ts
@@ -0,0 +1,72 @@
+export interface ProjectModel {
+    title: string
+}
+
+export interface Project {
+    id: number,
+    title: string
+    issues: object[]
+    created_at: Date
+    poster: string
+}
+
+export interface IssueModel {
+    title: string
+    description: string
+    solved: boolean
+    status: Status
+    label: Label
+    priority: Priority
+}
+
+export interface Issue {
+    id: number
+    title: string
+    description: string
+    solved: boolean
+    poster: string
+    status: Status
+    priority: Priority
+    label: Label
+    project_id: number
+    comments: Comment[]
+}
+
+export interface Comment {
+    id: number
+    content: string
+    poster: string
+    issue_id: number
+}
+
+export enum Status {
+    backlog,
+    todo,
+    in_progress,
+    done,
+    cancelled
+}
+
+export enum Label {
+    bug,
+    feature,
+    documentation,
+    needs_investigation,
+    question,
+    help_wanted
+}
+
+export enum Priority {
+    low,
+    medium,
+    high
+}
+
+export interface IProjects {
+    getAll(): Promise<any>
+    getOne(id: number): Promise<any>
+    createOne(model: ProjectModel): Promise<any>
+    updateOne(id: number, model: ProjectModel): Promise<any>
+    deleteOne(id: number): Promise<any>
+    newIssue(id: number, model: IssueModel): Promise<any>
+}
