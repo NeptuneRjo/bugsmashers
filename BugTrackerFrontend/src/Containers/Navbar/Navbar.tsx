858ffb4ed92700d1a468b4fc86d74abca21be320
NeptuneRjo <rjo9000@outlook.com>
2023-08-15 00:19:58 -0400

    create navbar container

---
+++ BugTrackerFrontend/src/Containers/Navbar/Navbar.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+
+function Navbar({ poster }: { poster: string | undefined }) {
+
+    const baseURL = process.env.REACT_APP_BASE_URL
+    const url = `${baseURL ? baseURL : "https://localhost:7104/"}signin`
+
+    const [providers, setProviders] = useState<any[]>([])
+
+    useEffect(() => {
+        ; (async () => {
+            const options: RequestInit = {
+                method: "GET"
+            }
+            const response = await fetch(url, options)
+
+            if (response.ok) {
+                const json = await response.json()
+                setProviders(json)
+            }
+
+        })()
+    }, [])
+
+    return (
+        <nav>
+            <h3><a href="/dashboard">Bug Smashers</a></h3>
+            <a href="/dashboard">Projects</a>
+            {poster === undefined ? (
+                <>
+                    {providers.map((provider, key) => (
+                        <form action={url} method="post" key={key}>
+                            <input type="hidden" name="Provider" value={provider.name} />
+                            <button type="submit">Connect using {provider.display_name}</button>
+                        </form>
+                    ))}
+                </>
+            ) : (
+                <a href="/profile">My Profile</a>
+            )}
+        </nav>
+    );
+}
+
+export default Navbar;
