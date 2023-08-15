7453bd45cfac5e08d70f97a853ce36d01a3b8c3e
NeptuneRjo <rjo9000@outlook.com>
2023-08-15 00:31:42 -0400

    exports

---
+++ BugTrackerFrontend/src/Views/exports.ts
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home/Home"
+export { default as Profile } from "./Profile/Profile"
+export { default as IssueDetails } from "./IssueDetails/IssueDetails"
+export { default as ProjectDetails } from "./ProjectDetails/ProjectDetails"
