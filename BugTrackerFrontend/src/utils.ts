8398080f523653dd6c3cd66947ca41aab6beb3b7
NeptuneRjo <rjo9000@outlook.com>
2023-08-16 16:26:57 -0400

    add util function for enum values

--- BugTrackerFrontend/src/utils.ts
+++ BugTrackerFrontend/src/utils.ts
@@ -7,3 +7,9 @@ export const formDataToObject = (formData: FormData) => {
 
     return formObject
 }
+
+export function getEnumValueByIndex<T extends object>(enumObj: T, index: number): T[keyof T] | undefined {
+    const enumValues = Object.values(enumObj) as Array<T[keyof T]>;
+    const enumValue = enumValues[index];
+    return enumValue;
+}
