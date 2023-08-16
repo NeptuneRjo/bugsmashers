4cc6a0c399cef2262a0d15b4765c41915f08a03a
NeptuneRjo <rjo9000@outlook.com>
2023-08-16 16:26:13 -0400

    create project details view

--- BugTrackerFrontend/src/Views/ProjectDetails/ProjectDetails.tsx
+++ BugTrackerFrontend/src/Views/ProjectDetails/ProjectDetails.tsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Label, Priority, Project, Status } from '../../types';
+import { instance } from '../../APIs/Projects';
+import { getEnumValueByIndex } from '../../utils';
 
 function ProjectDetails() {
-  return (
-    <p>Hello world!</p>
-  );
+
+    const { id } = useParams()
+
+    const [project, setProject] = useState<Project | undefined>(undefined)
+    const [loading, setLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+        ; (async () => {
+            const response = await instance.get(Number(id))
+
+            if (response.ok && response.data !== undefined) {
+                setProject(response.data)
+                setLoading(false)
+            }
+        })()
+    }, [])
+
+    if (loading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
+
+    return (
+        <div>
+            <h3>{project?.title.toUpperCase()}</h3>
+            <div>
+                <p>{project?.issues.length} Current Issues</p>
+                <a href={`/project/${project?.id}/new-issue`}>Create a new issue</a>
+            </div>
+            <hr />
+            <h4>Issues</h4>
+            <table>
+                <tr>
+                    <th>Title</th>
+                    <th>Label</th>
+                    <th>Poster</th>
+                    <th>Priority</th>
+                    <th>Status</th>
+                    <th>Comments</th>
+                </tr>
+                {project?.issues.map((issue, key) => (
+                    <tr key={key}>
+                        <td>{issue.title}</td>
+                        <td>{getEnumValueByIndex(Label, Number(issue?.label))}</td>
+                        <td>{issue.poster}</td>
+                        <td>{getEnumValueByIndex(Priority, Number(issue?.priority))}</td>
+                        <td>{getEnumValueByIndex(Status, Number(issue?.status))}</td>
+                        <td>{issue.comments.length}</td>
+                        <td><a href={`/issue/${issue.id}`}>Details</a></td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    )
+  
 }
 
 export default ProjectDetails;
