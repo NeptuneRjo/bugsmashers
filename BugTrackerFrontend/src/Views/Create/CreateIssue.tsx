bee0e6917e75b43f906db64482034c6305e8c336
NeptuneRjo <rjo9000@outlook.com>
2023-08-17 04:12:09 -0400

    Update to use indexing to map enum values instead of just mapping the enums

--- BugTrackerFrontend/src/Views/Create/CreateIssue.tsx
+++ BugTrackerFrontend/src/Views/Create/CreateIssue.tsx
@@ -3,10 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import IssueModel from '../../Models/IssueModel';
 import { Label, Priority, Status } from '../../types';
 import { instance } from "../../APIs/Projects"
-import { formDataToObject } from '../../utils';
-
-
-
+import { getEnumValueByIndex } from '../../utils';
 
 function CreateIssue({ poster }: { poster: string | undefined }) {
 
@@ -15,10 +12,16 @@ function CreateIssue({ poster }: { poster: string | undefined }) {
 
     const [title, setTitle] = useState<string>("")
     const [description, setDescription] = useState<string>("")
-    const [status, setStatus] = useState<Status>(Status.backlog)
-    const [label, setLabel] = useState<Label>(Label.bug)
-    const [priority, setPriority] = useState<Priority>(Priority.low)
+    const [status, setStatus] = useState<number>(0)
+    const [label, setLabel] = useState<number>(0)
+    const [priority, setPriority] = useState<number>(0)
     const [solved, setSolved] = useState<boolean>(false)
+
+    // Array of strings containing the keys of each enum...
+    // Used to map out the selects and their options
+    const statusKeys: string[] = Object.keys(Status)
+    const labelKeys: string[] = Object.keys(Label)
+    const priorityKeys: string[] = Object.keys(Priority)
 
     const handleSubmit = async (event: any) => {
         event.preventDefault()
@@ -49,25 +52,34 @@ function CreateIssue({ poster }: { poster: string | undefined }) {
             </div>
             <div>
                 <label htmlFor="status">Status</label>
-                <select name="status" value={status} onChange={(e) => setStatus(e.target.value as Status)}>
-                    {Object.values(Status).map((value, key) => (
-                        <option value={value} key={key}>{value}</option>
-                    )) }
+                <select name="status" value={status} onChange={(e) => setStatus(Number(e.target.value))}>
+                    {statusKeys.map((key, mapKey) => (
+                        // Map out the keys of the enum and set the
+                        // value to the index of this key in the array of enumKeys.
+                        // Extract the enum's property value using the index of the key.
+                        <option key={mapKey} value={statusKeys.indexOf(key)}>
+                            {getEnumValueByIndex(Status, statusKeys.indexOf(key))}
+                        </option>
+                    ))}
                 </select>
             </div>
             <div>
                 <label htmlFor="label">Label</label>
-                <select name="label" value={label} onChange={(e) => setLabel(e.target.value as Label)}>
-                    {Object.values(Label).map((value, key) => (
-                        <option value={value} key={key}>{value}</option>
-                    )) }
+                <select name="label" value={label} onChange={(e) => setLabel(Number(e.target.value))}>
+                    {labelKeys.map((key, mapKey) => (
+                        <option key={mapKey} value={labelKeys.indexOf(key)}>
+                            {getEnumValueByIndex(Label, labelKeys.indexOf(key))}
+                        </option>
+                    ))}
                 </select>
             </div>
             <div>
                 <label htmlFor="priority">Priority</label>
-                <select name="priority" value={priority} onChange={(e) => setPriority(e.target.value as Priority)}>
-                    {Object.values(Priority).map((value, key) => (
-                        <option value={value} key={key}>{value}</option>
+                <select name="priority" value={priority} onChange={(e) => setPriority(Number(e.target.value))}>
+                    {priorityKeys.map((key, mapKey) => (
+                        <option key={mapKey} value={priorityKeys.indexOf(key)}>
+                            {getEnumValueByIndex(Priority, priorityKeys.indexOf(key))}
+                        </option>
                     ))}
                 </select>
             </div>
