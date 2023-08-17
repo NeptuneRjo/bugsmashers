b28af6ed1343436a2669b0349559e36442165abb
NeptuneRjo <rjo9000@outlook.com>
2023-08-17 04:10:43 -0400

    add allowanymethods to cors

--- BugTrackerMvc/Program.cs
+++ BugTrackerMvc/Program.cs
@@ -59,6 +59,7 @@ builder.Services.AddCors(options =>
                           policy.WithOrigins("https://localhost:3000");
                           policy.AllowCredentials();
                           policy.WithHeaders("Content-Type");
+                          policy.AllowAnyMethod();
                       });
 });
 
