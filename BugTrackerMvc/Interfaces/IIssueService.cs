3a4aecfec2db7de490e9b553eb538887f3315b52
NeptuneRjo <rjo9000@outlook.com>
2023-08-17 04:10:20 -0400

    fix typings

--- BugTrackerMvc/Interfaces/IIssueService.cs
+++ BugTrackerMvc/Interfaces/IIssueService.cs
@@ -8,7 +8,7 @@ namespace BugTrackerMvc.Interfaces
         Task<IssueDto> GetIssue(int id);
         Task<Issue> GetIssue(int id, string poster);
         Task<ICollection<IssueDto>> GetIssues();
-        Task<ICollection<Issue>> GetIssues(string poster);
+        Task<ICollection<IssueDto>> GetIssues(string poster);
         Task<IssueDto> CreateIssue(string poster, IssueModel issueModel);
         Task<IssueDto> UpdateIssue(int id, string poster, IssueModel issueModel);
         Task<IssueDto> AddComment(int id, string poster, CommentModel commentModel);
