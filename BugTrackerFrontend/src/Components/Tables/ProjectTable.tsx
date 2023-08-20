793e7f6d99ca3b09c02997b1bf4896269b5f041a
NeptuneRjo <rjo9000@outlook.com>
2023-08-19 21:11:01 -0400

    style project table component

--- BugTrackerFrontend/src/Components/Tables/ProjectTable.tsx
+++ BugTrackerFrontend/src/Components/Tables/ProjectTable.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Project } from '../../types';
+import "../../Styles/ProjectTable.css"
+import moment from 'moment';
+import { search } from '../../utils';
 
 function ProjectTable({ projects }: { projects: Project[] }) {
+
+    const [searchParams] = useState<string[]>(["title"])
+    const [query, setQuery] = useState<string>("")
+
     return (
-        <table>
-            <tr>
-                <th>Title</th>
-                <th>Issues</th>
-                <th>Created At</th>
-            </tr>
-            {projects.map((project, key) => (
-                <tr key={key}>
-                    <td>{project.title}</td>
-                    <td>{project.issues.length}</td>
-                    <td>{project.created_at.split("T")[0]}</td>
-                    <td><a href={`/project/${project.id}`}>Details</a></td>
-                </tr>
-            ))}
-        </table>
+        <div id="projects">
+            <div id="projects-tools">
+                <input
+                    type="search"
+                    placeholder="Find a project"
+                    value={query}
+                    onChange={(event) => setQuery(event.target.value)}
+                />
+            </div>
+            <div id="projects-grid">
+                {search(projects, searchParams, query).map((project, key) => (
+                    <div key={key} id="project">
+                        <a href={`/project/${project.id}`}>{project.title}</a>
+                        <p>{project.issues.length} Issues</p>
+                        <span>Opened {moment(project.created_at).fromNow()} by {project.poster}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
