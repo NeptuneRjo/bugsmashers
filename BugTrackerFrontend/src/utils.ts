1fb535d6f057d9b9cbebe15a29d58014e7790858
NeptuneRjo <rjo9000@outlook.com>
2023-08-19 21:11:28 -0400

    add util function for sorting through array

--- BugTrackerFrontend/src/utils.ts
+++ BugTrackerFrontend/src/utils.ts
@@ -13,3 +13,16 @@ export function getEnumValueByIndex<T extends object>(enumObj: T, index: number)
     const enumValue = enumValues[index];
     return enumValue;
 }
+
+export const search = <T extends object, K extends keyof T>(items: T[], searchParams: string[], query: string) => {
+    return items.filter((item) => {
+        return searchParams.some((newItem) => {
+            return (
+                (item[newItem as K] as string)
+                    .toString()
+                    .toLowerCase()
+                    .indexOf(query.toLocaleLowerCase()) > -1
+            )
+        })
+    })
+}
