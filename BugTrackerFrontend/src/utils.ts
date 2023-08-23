f4f08b2a75d161e32ee5a65758154f52d44b44e7
NeptuneRjo <rjo9000@outlook.com>
2023-08-23 19:10:45 -0400

    clean utils

--- BugTrackerFrontend/src/utils.ts
+++ BugTrackerFrontend/src/utils.ts
@@ -1,19 +1,3 @@
-export const formDataToObject = (formData: FormData) => {
-    let formObject: any = {}
-
-    for (const [key, value] of Object.entries(formData)) {
-        formObject[key] = value
-    }
-
-    return formObject
-}
-
-export function getEnumValueByIndex<T extends object>(enumObj: T, index: number): T[keyof T] | undefined {
-    const enumValues = Object.values(enumObj) as Array<T[keyof T]>;
-    const enumValue = enumValues[index];
-    return enumValue;
-}
-
 export const search = <T extends object, K extends keyof T>(items: T[], searchParams: string[], query: string) => {
     return items.filter((item) => {
         return searchParams.some((newItem) => {
