36d505d936f860248c8ddc38c6b413333e198922
NeptuneRjo <rjo9000@outlook.com>
2023-08-24 04:05:13 -0400

    export auth feature

--- BugTrackerFrontend/src/APIs/features.ts
+++ BugTrackerFrontend/src/APIs/features.ts
@@ -1,7 +1,9 @@
 import Issues from "./Features/Issues";
 import Projects from "./Features/Projects";
+import Auth from "./Features/Auth"
 
 export default {
     Issues,
-    Projects
+    Projects,
+    Auth
 }
