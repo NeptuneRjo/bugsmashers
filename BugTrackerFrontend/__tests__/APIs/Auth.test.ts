ed68abf4bef310530aa36b3196c977a4b80ffbe5
NeptuneRjo <rjo9000@outlook.com>
2023-08-25 01:19:40 -0400

    create tests for auth feature

---
+++ BugTrackerFrontend/__tests__/APIs/Auth.test.ts
@@ -0,0 +1,53 @@
+import Service from "../../src/APIs/apiService"
+import Auth from "../../src/APIs/Features/Auth"
+import { jest } from '@jest/globals';
+
+jest.mock("../../src/APIs/apiService")
+
+let requestMock: jest.Mock
+let mockService: Service
+
+beforeEach(() => {
+    (Service as jest.Mock).mockClear(); // Clear mock implementation
+
+    requestMock = jest.fn();
+    requestMock.mockReturnValue('return');
+
+    (Service as jest.Mock).mockImplementation(() => ({
+        request: requestMock,
+    }));
+
+    mockService = new Service('foo');
+});
+
+describe("Auth", () => {
+    describe("list", () => {
+        it("proxies the request method", () => {
+            const auth = new Auth(mockService)
+            const returnValue = auth.list()
+
+            expect(requestMock).toHaveBeenLastCalledWith("authentication", "get", null)
+            expect(returnValue).toBe("return")
+        })
+    })
+
+    describe("retrieve", () => {
+        it("proxies the request method", () => {
+            const auth = new Auth(mockService)
+            const returnValue = auth.retrieve()
+
+            expect(requestMock).toHaveBeenLastCalledWith("authentication/user", "get", null, true)
+            expect(returnValue).toBe("return")
+        })
+    })
+
+    describe("logout", () => {
+        it("proxies the request method", () => {
+            const auth = new Auth(mockService)
+            const returnValue = auth.logout()
+
+            expect(requestMock).toHaveBeenLastCalledWith("authentication/signout", "get", null, true)
+            expect(returnValue).toBe("return")
+        })
+    })
+})
