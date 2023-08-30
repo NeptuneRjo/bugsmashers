3d21590af595f2a9b90d9a4977e2981ab1b5ce4b
NeptuneRjo <rjo9000@outlook.com>
2023-08-30 17:00:15 -0400

    migrations

---
+++ BugTrackerMvc/Migrations/20230830093503_IssueStats.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugTrackerMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class IssueStats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Issues",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Issues",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Issues");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "Issues");
+        }
+    }
+}
