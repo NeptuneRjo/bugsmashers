485f801cc8a3faa56c85354f4dc34a126328c0f5
NeptuneRjo <rjo9000@outlook.com>
2023-12-07 16:03:27 -0500

    Implement authentication controller

--- BugTracker.API/Controllers/AuthenticationController.cs
+++ BugTracker.API/Controllers/AuthenticationController.cs
@@ -1,5 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BugTracker.BLL.Utilities.Extensions;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BugTracker.API.Controllers
 {
@@ -7,5 +16,104 @@ namespace BugTracker.API.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        private IConfiguration _configuration;
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            ICollection<AuthenticationScheme> providers = await HttpContext.GetExternalProvidersAsync();
+
+            return Ok(providers);
+        }
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> Post([FromForm] string provider)
+        {
+            // Note: the "provider" parameter corresponds to the external
+            // authentication provider choosen by the user agent.
+            if (string.IsNullOrWhiteSpace(provider))
+            {
+                return BadRequest();
+            }
+
+            if (!await HttpContext.IsProviderSupportedAsync(provider))
+            {
+                return BadRequest();
+            }
+
+            // Instruct the middleware corresponding to the requested external identity
+            // provider to redirect the user agent to its own authorization endpoint.
+            // Note: the authenticationScheme parameter must match the value configured in Startup.cs
+            return Challenge(new AuthenticationProperties { RedirectUri = "/api/authentication/signin-callback" }, provider);
+        }
+
+        [HttpGet("signin-callback")]
+        [Authorize]
+        public async Task<IActionResult> Callback()
+        {
+            string token = CreateToken();
+
+            string redirectUrl = "https://localhost:3000/login?token=" + token;
+
+            return Redirect(redirectUrl);
+        }
+
+        [HttpGet("user")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public IActionResult GetUser()
+        {
+            string username = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "username")?.Value;
+
+            if (username != null)
+            {
+                var user = new Dictionary<string, string>()
+                {
+                    { "username", username }
+                };
+
+                return Ok(user);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpGet("signout")]
+        [HttpPost("signout")]
+        public IActionResult SignOutCurrentUser()
+        {
+            // Instruct the cookies middleware to delete the local cookie created
+            // when the user agent is redirected from the external identity provider
+            // after a successful authentication flow (e.g Google or Facebook).
+            return SignOut(new AuthenticationProperties { },
+                CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        private string CreateToken()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+
+            string username = User.FindFirst(ClaimTypes.Name)?.Value;
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("username", username)
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
     }
 }
