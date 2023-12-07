1e7412566e949a24cb3cd09db099e6bd82167b6f
NeptuneRjo <rjo9000@outlook.com>
2023-12-07 14:14:01 -0500

    Add auth policies and cors

--- BugTracker.BLL/DependencyInjection.cs
+++ BugTracker.BLL/DependencyInjection.cs
@@ -1,8 +1,12 @@
 ﻿using BugTracker.BLL.Services;
 using BugTracker.BLL.Services.IServices;
 using BugTracker.BLL.Utilities.AutoMapper;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace BugTracker.BLL
 {
@@ -14,6 +18,53 @@ namespace BugTracker.BLL
             services.AddScoped<IProjectService, ProjectService>();
 
             services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+            // Authentication Policies
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddCookie()
+               .AddJwtBearer(options =>
+               {
+                   SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]));
+
+                   options.RequireHttpsMetadata = false;
+                   options.SaveToken = true;
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuerSigningKey = true,
+                       IssuerSigningKey = key,
+                       ValidateIssuer = false,
+                       ValidateAudience = false
+                   };
+               })
+               .AddGitHub(options =>
+               {
+                   options.ClientId = Configuration["GitHub:ClientId"];
+                   options.ClientSecret = Configuration["GitHub:ClientSecret"];
+               });
+
+            services.AddAuthorization();
+
+            // CORs
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "_myAllowSpecificOrigins",
+                                  policy =>
+                                  {
+                                      policy.WithOrigins(
+                                          "https://localhost:3000", 
+                                          "http://localhost:3000");
+                                      policy.AllowCredentials();
+                                      policy.AllowAnyMethod();
+                                      policy.AllowAnyHeader();
+                                  });
+            });
         }
     }
 }
